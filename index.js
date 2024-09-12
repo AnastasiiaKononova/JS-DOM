eda798ee42cbff85c8b85e1441423a384fb940b6
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-09-12 16:41:21 +0300

    Add task resolve

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
-// // DOM
-
-// /*
-// Document
-// Object
-// Model
-// (Об'єктна модель документа) - це представлення документу як набору елементів-об'єктів, інтерфейс для взаємодії з документом.
-// Вхідна точка DOM - document
-// */
-
-
-// /*
-// Два важливих принципи:
-// 1. Все, що нас оточує - це об'єкти.
-
 /*
-Об'єкт змінюється за посиланням (і передається теж), вплив на об'єкт змінює той елемент, який цим об'єктом представлений.
-У об'єктів є властивості та методи.
-Об'єкти можуть ділитися властивостями та методами зі своїми нащадками за допомогою прототипного наслідування
+Створити пусту сторінку (без елементів).
+Динамічно створити секцію, всередині якої параграф і кнопка. В параграфі та кнопці довільний текст
 */
-
-const elem = document.createElement('button');
-elem.textContent = 'Click me please'
-
-/*
-Створення елементів:
-1. Створити об'єкт потрібного елемента
-2. Вказати батьківському елементу, що цей стане його дитиною
-    Для цього потрібно мати доступ до елемента (батька), який вже є на сторінці
-*/
-
-const element = document.createElement('p');  // створюється HTMLParagraphElement
-
-element.textContent = 'PARAGRAPH element';
-element.style.color = 'violet';
 
 const body = document.body;
+
 body.append(element);
+const p = document.createElement('p');
+p.textContent = 'Довільний текст';
+
+const btn = document.createElement('button');
+btn.textContent = 'Клацни мене';
+
+const section = document.createElement('section');
+section.append(p, btn);
+
+body.append(section);
