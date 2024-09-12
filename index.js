749b987a8a03a832aa1977b2a8bec2b64d862ada
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-09-12 13:13:06 +0300

    task resolve

--- index.js
+++ index.js
@@ -8,64 +8,45 @@
 // Вхідна точка DOM - document
 // */
 
-// console.log(1);
+console.log(1);
 
-// const p = document.getElementById('super-par');
-// console.log(p);
-// p.style="color: green";
-
-
-// const paragraphs = document.getElementsByTagName('p');
-// console.log(paragraphs)  /// HTMLCollection - псевдо-масив (ітерована колекція, яка не є екземпляром Array)
-
-// for (const element of paragraphs) {
-//     element.style = 'color: violet';
-// }
-
-// const array = [...paragraphs]; // перетворюємо її на звичайний масив, що дає нам можливість використовувати методи масива
+const p = document.getElementById('super-par');
+console.log(p);
+p.style="color: green";
 
 
+const paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs)  /// HTMLCollection - псевдо-масив (ітерована колекція, яка не є екземпляром Array)
 
-// const redClassP = document.getElementsByClassName('red-text');
+for (const element of paragraphs) {
+    element.style = 'color: violet';
+}
 
-// console.log(redClassP);
+const array = [...paragraphs]; // перетворюємо її на звичайний масив, що дає нам можливість використовувати методи масива
+
+
+
+const redClassP = document.getElementsByClassName('red-text');
+
+console.log(redClassP);
 
 // /*
 // Два важливих принципи:
 // 1. Все, що нас оточує - це об'єкти.
 
+/*
+Об'єкт змінюється за посиланням (і передається теж), вплив на об'єкт змінює той елемент, який цим об'єктом представлений.
+У об'єктів є властивості та методи.
+Об'єкти можуть ділитися властивостями та методами зі своїми нащадками за допомогою прототипного наслідування
+*/
 
 
-// Доступ до елементів
+/* Створити на сторінці article і всередині нього 5 p з текстом.
+1. Динамічно пофарбувати текст у червоний колір
+2. Видалити останній параграф
+*/
 
-console.log(document);
-console.log(document.head);
-console.log(document.body);
+paragraphs[paragraphs.length - 1].remove();
 
-// document.querySelector(<selector>)
-// document.querySelectorAll(<selector>)
-// document.getElementById(<id>)
-// document.getElementsByClassName(<class>)
-// document.getElementsByTagName(<tag>)
 
-const rootEl = document.getElementById('root');
-console.log('rootEl :>> ', rootEl);
-console.dir(rootEl);
 
-const divEls = document.getElementsByTagName('div');
-console.log('divEls :>> ', divEls);
-
-const divRoot2 = divEls[0];
-
-const contentDivs = document.getElementsByClassName('content');
-console.log('contentDivs :>> ', contentDivs);
-
-const rootEl3 = document.querySelector('#root');
-console.log('rootEl3 :>> ', rootEl3);
-
-const divEls1 = document.querySelectorAll('div');
-console.log('divEls1 :>> ', divEls1);
-
-// Отримати список елементів з класом content
-const contentEls = document.querySelectorAll('.content');
-console.log(contentEls);
