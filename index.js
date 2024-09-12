86c222d9aa3d79a8e525e18bffcbaaf77633ed2d
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-09-12 13:10:11 +0300

    Add methods DOM Vi

--- index.js
+++ index.js
@@ -1,36 +1,71 @@
-// DOM
+// // DOM
 
-/*
-Document
-Object
-Model
-(Об'єктна модель документа) - це представлення документу як набору елементів-об'єктів, інтерфейс для взаємодії з документом.
-Вхідна точка DOM - document
-*/
+// /*
+// Document
+// Object
+// Model
+// (Об'єктна модель документа) - це представлення документу як набору елементів-об'єктів, інтерфейс для взаємодії з документом.
+// Вхідна точка DOM - document
+// */
 
-console.log(1);
+// console.log(1);
 
-const p = document.getElementById('super-par');
-console.log(p);
-p.style="color: green";
-
-
-const paragraphs = document.getElementsByTagName('p');
-console.log(paragraphs)  /// HTMLCollection - псевдо-масив (ітерована колекція, яка не є екземпляром Array)
-
-for (const element of paragraphs) {
-    element.style = 'color: violet';
-}
-
-const array = [...paragraphs]; // перетворюємо її на звичайний масив, що дає нам можливість використовувати методи масива
+// const p = document.getElementById('super-par');
+// console.log(p);
+// p.style="color: green";
 
 
+// const paragraphs = document.getElementsByTagName('p');
+// console.log(paragraphs)  /// HTMLCollection - псевдо-масив (ітерована колекція, яка не є екземпляром Array)
 
-const redClassP = document.getElementsByClassName('red-text');
+// for (const element of paragraphs) {
+//     element.style = 'color: violet';
+// }
 
-console.log(redClassP);
+// const array = [...paragraphs]; // перетворюємо її на звичайний масив, що дає нам можливість використовувати методи масива
 
-/*
-Два важливих принципи:
-1. Все, що нас оточує - це об'єкти.
-*/
+
+
+// const redClassP = document.getElementsByClassName('red-text');
+
+// console.log(redClassP);
+
+// /*
+// Два важливих принципи:
+// 1. Все, що нас оточує - це об'єкти.
+
+
+
+// Доступ до елементів
+
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+
+// document.querySelector(<selector>)
+// document.querySelectorAll(<selector>)
+// document.getElementById(<id>)
+// document.getElementsByClassName(<class>)
+// document.getElementsByTagName(<tag>)
+
+const rootEl = document.getElementById('root');
+console.log('rootEl :>> ', rootEl);
+console.dir(rootEl);
+
+const divEls = document.getElementsByTagName('div');
+console.log('divEls :>> ', divEls);
+
+const divRoot2 = divEls[0];
+
+const contentDivs = document.getElementsByClassName('content');
+console.log('contentDivs :>> ', contentDivs);
+
+const rootEl3 = document.querySelector('#root');
+console.log('rootEl3 :>> ', rootEl3);
+
+const divEls1 = document.querySelectorAll('div');
+console.log('divEls1 :>> ', divEls1);
+
+// Отримати список елементів з класом content
+const contentEls = document.querySelectorAll('.content');
+console.log(contentEls);
