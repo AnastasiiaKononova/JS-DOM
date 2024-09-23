9ad8cacde9d518651f228c3f22da3c3ca6668598
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-09-23 19:26:31 +0300

    Add form submit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-//Data-* атрибути 
+const form = document.querySelector('form');
 
-// За натиснення на кнопку фоновий колір кнопки має стати таким, як написано у атрибуті data-color
-const btns = document.querySelectorAll('button');
-btns.forEach(btn => {
-    btn.addEventListener('click', function({target}){
-        target.classList.add(target.dataset.class);
-    })
+/*
+<form> за замовчуванням має специфічну поведінку - вона перезавантажує сторінку, намагаючись самостійно відправити дані туди, куди вказує атрибут action
+*/
+
+form.addEventListener('submit', function(event){
+    // потрібно зупинити поведінку за замовчуванням!
+    event.preventDefault(); // виклик методу зупиняє поведінку за-замовчуванням
+    const form = event.target;
+    const inputUsername = form.username.value;
+    const inputPass = form.pass.value;
+    console.log({inputUsername, inputPass})
 })
-
