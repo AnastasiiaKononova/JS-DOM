331de95e711e4e1c9d6397c2dc52f662b0c6fbb0
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-10-07 12:26:28 +0300

    timeout eventLoop

--- index.js
+++ index.js
@@ -31,11 +31,24 @@ console.log('Javascript working');
  setTimeout, setInterval
 Задачі, відкладені у часі
 */
-setTimeout(tickTack, 1000);
-function tickTack(){
-    console.log('tick-tack')
-}
+
 /*
 setTimeout(fn, time) - приймає посилання на функцію і через вказаний час запускає її на виконання
 Час отстрочки - МІНІМАЛЬНИЙ ЧАС, оскільки через цей вказаний проміжок часу задача буде переміщена у чергу коллбеків і буде чекати на виконання, коли стек буде пустим. 
 */
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function onClick() {
+    setTimeout(function timer() {
+        console.log('You clicked the button!');    
+    }, 2000);
+});
+console.log("Hi!");
+setTimeout(function timeout() {
+    console.log("timer!");
+}, 5000);
+console.log("Welcome to loupe.");
+for(let i=0; i< 1000000000; i++){
+    
+}
+console.log('hard loop end');
