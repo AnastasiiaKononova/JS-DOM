8679c2b3f6ff2d7fd75f8b3e50b69676327dda0f
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-10-21 18:38:42 +0300

    Add ES-6 modules

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
+// іменований імпорт -----------------
+// import { sum as mySum, mult, PI } from './math.js';
 
-/*
-Таска: 
-1. Зареєструватись на https://home.openweathermap.org/
-2. Згенерувати ключ (API key) і зберегти його в коді як константу
-3. Дока: https://openweathermap.org/current
-4. За відправки форми зробити запит на АПІ з обраним містом.
-За результатом запиту відобразити користувачу погоду у вигляді:
-Місто: ....
-Погода: ...
-Температура: ...
-*/
+// console.log('sum(1,2) :>> ', mySum(1, 2));
+// console.log('mult(PI,2) :>> ', mult(PI, 2));
 
-// example https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric
-const form = document.querySelector('form');
-form.addEventListener('submit', formSubmit);
-function formSubmit(event){
-    event.preventDefault();
-    const city = event.target.city.value;
-    loadWeatherData(city)
-}
-async function loadWeatherData(city) {
-    const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=uk`;
-    const response = await fetch(requestURL);
-    const weatherData = await response.json();
-    createWeatherCard(weatherData)
-}
-function createWeatherCard(weatherObj) {
-    const h3 = createElement('h3', undefined, 'Місто: ', weatherObj.name);
-    const p = createElement('p', [], 'Погода: ', weatherObj.weather[0].description);
-    const tempP = createElement('p', [], 'Температура: ', weatherObj.main.temp);
-    const article = createElement('article', [], h3, p, tempP);
-    const root = document.querySelector('#root');
-    root.replaceChildren(article)
-}
-function createElement(type, classNames=[], ...children) {
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-    return elem;
-}
+// або
+// import * as MyMath from './math.js';
+
+// console.log('MyMath.sum(PI,3) :>> ', MyMath.sum(MyMath.PI, 3));
+
+// дефолтний імпорт -----------------------
+// import MyClass from './math.js';
+
+// console.log('new MyClass(5) :>> ', new MyClass(5));
+
+// поэднаний варіант -------------------------
+
+// import { default as MyTest, sum, mult, PI } from './math.js';
+import MyTest, { sum, mult, PI } from './math.js';
+
+console.log('mult(PI,2) :>> ', mult(PI, 2));
+console.log('new MyTest(5) :>> ', new MyTest(5));
