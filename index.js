590dd1d763b08667431106a1822e50b3016465eb
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-10-21 13:55:18 +0300

    promisifying work

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
-// Promise
+setTimeout(()=>{
+    //    console.log('tick')
+    }, 1000);
 
-/* Особливий об'єкт, який використовується для асинхронних обчислень
- Дає можливість "підписуватись" на зміни в асинхронних процесах та узгоджувати асинхронні дії, дає можливість уникнути "callback hell"
- Проміс має стан, який може бути 1 з 3:
- - pending (очікування, обробка) 
-або
-    - fullfiled (success) - успішне завершення асинхронної дії
-або 
-    - rejected (error) - неуспішне виконання асинхронної дії. Необроблена (або другим коллбеком then, або catch) помилка у промісі вивалюється як Uncaught (in promise) error
+    delay(3000).then(()=>{
+        //    console.log('проміс відкладений у часі')
+        })
+        
+        function delay(ms){
+            return new Promise((resolve, reject)=>{
+                setTimeout(resolve, ms)
+            })
+        }
 
-    Стан може бути тільки один в 1 момент часу. Після набуття другого статусу (fullfiled/rejected) більше ніякої зміни статусу не станеться
+        const promise = delay(2000);
+promise.then(function(){
+//    console.log('tick-tack!')
+})
 
- Проміс може отримати коллбеки для обробки результату за допомогою метода then
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('button click!');
+})
+
+const imagePath = 'https://d19h8kn98xvxar.cloudfront.net/images/_hero/connectwithnature.jpg';
+const invalidPath = 'http://82743k2jhk234mnsdfkjhs';
+
+function loadImage(src){
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
+    img.addEventListener('load', function(){
+        // ось це виконується тоді, коли ВСЯ каринка вже завантажилась
+        document.body.append(img);
+    });
+    img.addEventListener('error', function(){
+        console.log('oops!')
+    })
+    return img
+}
+
+loadImage(imagePath)
+console.log('пішла вантажитись')
 */
-
-new Promise(function(resolve, reject) {
-    // в разі успішно виконаної роботи викликаємо resolve()
-    // проміс розуміє, що в нього статус fullfiled і коллбеку передається інформація (дані проміса)
-
-    // в разі неуспішного виконання роботи або помилки - викликаємо reject()
-    // проміс розуміє, що у нього статус rejected і передає коллбеку інфу
+function loadImage(src){
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
+    return new Promise((resolve, reject) =>{
+        img.addEventListener('load', () => {
+            resolve(img)
+        });
+        img.addEventListener('error', () => {
+            reject(img)
+        })
+    })
+}
+loadImage(imagePath)
+.then((img) => {
+    document.body.append(img)
 })
-
-/*
-.then(function resolve(data){
-    // ось тут в data доступні дані проміса
-}, function reject(error){
-    // ось тут в error доступні дані проміса
+.catch((img) => {
+    const placeholder = document.createElement('div');
+    placeholder.classList.add('placeholder');
+    document.body.append(placeholder)
 })
-
-Або:
-
-.then(function resolve(data){
-    // ось тут в data доступні дані проміса
-})
-
-.catch(function reject(error){
-    // ось тут в error доступні дані проміса
-})
-*/
